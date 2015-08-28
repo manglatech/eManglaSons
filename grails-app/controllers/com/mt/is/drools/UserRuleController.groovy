9784741029d0913fcc5b8c128a808c0f828af519
manglatech <dpatel@integralads.com>
2015-08-28 13:09:17 -0400

    Drools running rule - Partial

--- grails-app/controllers/com/mt/is/drools/UserRuleController.groovy
+++ grails-app/controllers/com/mt/is/drools/UserRuleController.groovy
@@ -27,10 +27,17 @@ class UserRuleController {
 		model.results["applicationStatelessSession - my user name is $user.username (true)"] = user.valid
 		*/
 		
+		def nirali = new User("nirali", "nirali");
+		Object[] facts1 = [nirali]
+		droolsService.executeFromFile("rules/user.drl", [nirali])
+		model.results["applicationStatelessSession - my user name is $nirali.username (false)"] = nirali.username
+		
+		println "nirali Rule"
+		
 		def user = new User("dhaval", "dhaval");
 		Object[] facts = [user]
 		droolsService.executeFromFile("rules/user.drl", [user])
-		model.results["applicationStatelessSession - my user name is $user.username (true)"] = user.valid
+		model.results["applicationStatelessSession - my user name is $user.username (true)"] = user.username
 		
-	}
+		}
 }
